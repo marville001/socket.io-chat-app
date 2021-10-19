55b580981da8400161cde79eb230eeab15d2edb3
marville001 <mwangimartin1904@gmail.com>
2021-10-19 18:40:27 +0300

    connect app with server

--- client/src/pages/ChatRoom.jsx
+++ client/src/pages/ChatRoom.jsx
@@ -1,17 +1,39 @@
-import { Grid } from '@mui/material'
-import React from 'react'
-import ChatArea from '../components/ChatArea'
-import LeftSidebar from '../components/LeftSidebar'
-import RightSidebar from '../components/RightSidebar'
+import { Grid } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import queryString from "query-string";
+import {io} from "socket.io-client";
 
-const ChatRoom = () => {
-    return (
-        <Grid container spacing={0}>
-        <LeftSidebar />
-        <ChatArea />
-        <RightSidebar />
-      </Grid>
-    )
-}
+import ChatArea from "../components/ChatArea";
+import LeftSidebar from "../components/LeftSidebar";
+import RightSidebar from "../components/RightSidebar";
 
-export default ChatRoom
+let socket;
+
+const ChatRoom = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  const ENDPOINT = 'http://localhost:4000'
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT)
+
+    setName(name);
+    setRoom(room);
+
+    socket.emit('user-joined', {name, room})
+
+  }, [ENDPOINT, location.search]);
+
+  return (
+    <Grid container spacing={0}>
+      <LeftSidebar />
+      <ChatArea />
+      <RightSidebar />
+    </Grid>
+  );
+};
+
+export default ChatRoom;
--- server/index.js
+++ server/index.js
@@ -1,14 +1,29 @@
-const app  = require("express")();
 const http = require('http')
+const cors = require('cors')
+const app  = require("express")();
+app.use(cors())
 const server = http.createServer(app)
+const {Server} = require('socket.io')
 
-const io = require('socket.io')(server)
+
+
+
+const io = new Server(server, {
+    cors:{
+        origin: "http://localhost:3000",
+        methods:['GET', "POST"]
+    }
+})
 
 io.on('connection', socket=>{
-    console.log("New connection!!!!");
-    // socket.on('message', ({name, message})=>{
-    //     io.emit('message', {name, message})
-    // })
+    console.log(socket.id);
+    socket.on('message', ({name, message})=>{
+        io.emit('message', {name, message})
+    })
+
+    socket.on('user-joined', ({name, room})=>{
+        
+    })
 
     socket.on('disconnect', ()=>{
         console.log('User has left!!!');
