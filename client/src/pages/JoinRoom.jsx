eccede2bb3aa3159fe013b7688453aa94c7c386f
marville001 <mwangimartin1904@gmail.com>
2021-10-19 18:13:44 +0300

    Finich join room

--- client/src/pages/JoinRoom.jsx
+++ client/src/pages/JoinRoom.jsx
@@ -1,30 +1,42 @@
 import { styled } from "@mui/system";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { TextField, Button } from "@mui/material";
 
 const JoinRoom = () => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
-  
-  const handleJoinRoom = ()=>{
-      console.log({name, room});
-  }
+
+  const history = useHistory()
+
+  const handleJoinRoom = () => {
+    if(name && room){
+        history.push(`/chat?name=${name}&room=${room}`)
+    }
+  };
 
   return (
     <Container>
       <JoinForm>
         <h1>Join Room</h1>
-        <TextInput onChange={e=>setName(e.target.value)} value={name} label="Enter your Name" />
-        <TextInput onChange={e=>setRoom(e.target.value)} value={room} label="Enter room" />
-        <Button
-          style={{ width: "100%", marginTop: "20px" }}
-          color="primary"
-          variant="contained"
-          onClick={handleJoinRoom}
-        >
-          Join Room
-        </Button>
+        <TextInput
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          label="Enter your Name"
+        />
+        <TextInput
+          onChange={(e) => setRoom(e.target.value)}
+          value={room}
+          label="Enter room"
+        />
+          <Button
+            style={{ width: "100%", marginTop:"20px" }}
+            variant="contained"
+            color="primary"
+            onClick={handleJoinRoom}
+          >
+            Join Room
+          </Button>
       </JoinForm>
     </Container>
   );
