ddb71ade21cfdd08e9c5f5dc352e4ab84a193878
marville001 <mwangimartin1904@gmail.com>
2021-10-19 21:09:49 +0300

    Make it better

--- server/users.js
+++ server/users.js
@@ -4,6 +4,8 @@ const addUser = ({id, name, room})=>{
     name = name.trim().toLowerCase();
     room = room.trim().toLowerCase();
 
+    if(!name || !room) return { error: 'Username and room are required.' };
+
     const existingUser = users.find(user=>user.room === room && user.name === name);
 
     if(existingUser) return {error:"Username is taken"}
